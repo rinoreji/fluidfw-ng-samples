9f3dca39511a21d5ac6da28bcfa0c02632daec56
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2022-05-04 01:57:49 +0530

    removed tinylicious client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StickyNotesComponent } from './sticky-notes/sticky-notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StickyNotesComponent
   ],
   imports: [
     BrowserModule,
